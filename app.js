d6a04aa49cf382883be00cd0484f2880b81c5921
azotamiota <azotamiota@gmail.com>
2022-09-11 20:46:21 +0100

    update server

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
 const cors = require('cors')
 const http = require('http')
 const {Server} = require('socket.io')
@@ -7,31 +6,36 @@ require('dotenv').config()
 app.use(cors())
 
 app.get('/', (reg, res) => {
-    res.json({message: 'Welcome to millionare chat app'})
+    res.json({message: 'Welcome to my simple chat app'})
 })
 
 const server = http.createServer(app)
 
+// const url = "https://chat-simple.netlify.app"
+const url = "http://localhost:5173"
+
 const io = new Server(server, {
     cors: {
-        origin: "https://chat-simple.netlify.app",
+        origin: url,
         methods: ['GET', 'POST']
     }
 })
 
 io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`);
-    socket.on('disconnect', () => {
-        console.log(`User ${socket.id} disconnected`);
-    })
+
     socket.on("send_message", (data) => {
         console.log('data arrived, to be emitted: ', data);
         io.emit('receive_message', data)
     })
+
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+    })
 })
 
 const port = process.env.PORT || 3000
-console.log(port)
+
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
